45f253b06e2bf4f4d79e35a73c28359a3f02fe31
Afshar <afsharali8734@gmail.com>
2025-07-08 20:35:44 +0530

    Deleted Day 1 to Day 10 folders

---
+++ Day 15 JS/apshar.js
@@ -0,0 +1,103 @@
+//operators
+//  let age = 18;
+// console.log(age <= 18) // false
+
+// conditional statement
+
+// console.log("before my if statement");
+// let age = 23;
+// if (age >= 18) {
+//     console.log("you can vote")
+//     console.log("you can drive")
+//   let a = 5;
+//   console.log(5 * a);
+// }
+
+
+// console.log("after my if statement");
+
+// if (age > 20) {
+//     console.log("you are in your 20s")
+// }
+
+// let firstName = "Afshar"
+//    if (firstName == "Afshar") {
+//      console.log(`Welcome ${firstName}`)
+//    }
+
+// Traffic light system
+
+// let color = "green"
+// if (color === "red") {
+//   console.log("Stop! light color is red");
+// }
+// else if (color === "yellow"){
+//   console.log("Slow down. light color is yellow")
+// }
+// else if (color === "green") {
+//   console.log("Go. light color is green")
+// }
+
+// let age = "14"
+// if (age >= 18){
+//   console.log("you can vote")
+// }
+// else if (age >= 18){
+//   console.log("you cannot vote")
+// }
+// else if (age < 18){
+//   console.log("you cannot vote2")
+// }
+
+// Grading system develop
+
+// let marks = 90;
+// if (marks >= 80) {
+//   console.log("A+")
+// }
+// else if (marks >= 60) {
+//   console.log("A")
+// }
+// else if (marks >= 33) {
+//   console.log("B")
+// }
+// else if (marks < 33) {
+//   console.log("F")
+// }
+
+// Seasons example
+
+let months = "april";
+if (months === "january") {
+  console.log("winter is here")
+}
+else if (months === "april") {
+  console.log("summer is here")
+}
+
+// Else statement
+
+let age = "17";
+if (age >= 18) {
+  console.log("you can vote")
+}
+else {
+  console.log("you cannot vote")
+}
+
+// Traffic light 2 (else)
+
+let color = "white"
+if(color === "red"){
+  console.log("Stop!")
+}
+else if(color === "yellow"){
+  console.log("Slow down.")
+}
+else if(color === "green"){
+  console.log("Go.")
+}
+else {
+  console.log("Traffic light is broken")
+}
+
---
+++ Day 16 JS/app.js
@@ -0,0 +1,61 @@
+// let msg = "   he llo   "
+// msg.trim();
+
+// let password = prompt("set your password")
+// let newPass = password.trim();
+// console.log(password);
+
+// let name = "Afshar Ali";
+
+// let msg = "error"
+
+// console.log(msg.toUpperCase());
+
+
+//methods with arguments  ( index of )
+
+// let msg = "ILoveCoding"
+
+
+//method chaining
+
+// let msg = "    hello    "
+// let newMsg = msg.trim();
+// console.log("after trim :", newMsg);
+// newMsg = newMsg.toUpperCase();
+// console.log("after UpperCase :", newMsg);
+
+// let newMsg = msg.trim().toUpperCase();
+// console.log(newMsg);
+
+//slice method
+
+// let msg = "apnacollege"
+// console.log(msg.slice(0, 4));
+
+// let msg = "apnacollege"
+// console.log(msg.slice(4));
+
+// slice (-num) 
+
+// let msg = "apnacollege"
+// console.log(msg.slice(-15)) // 11-1 => 10
+
+// Replace method
+
+// let msg = "ILoveCoding";
+
+//repeat method
+// let fruit = "mango"
+// console.log(fruit.repeat(5));
+
+
+//Practice Question 1
+
+// let msg = "hello!"
+// console.log(msg.trim().toUpperCase());
+
+// Qs 2
+
+let name = "ApnaCollege"
+console.log(name.slice(4, 9))
